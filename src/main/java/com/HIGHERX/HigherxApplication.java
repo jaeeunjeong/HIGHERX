d753a85dacc27db0c84d0fafe820bd9404814347
jaeeunjeong <jejeong000@gmail.com>
2023-05-17 22:25:23 +0900

    add : init commit

---
+++ src/main/java/com/HIGHERX/HigherxApplication.java
@@ -0,0 +1,13 @@
+package com.HIGHERX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HigherxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HigherxApplication.class, args);
+	}
+
+}
